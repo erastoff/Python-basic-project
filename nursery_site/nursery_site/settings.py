0030cd1cf48e1a398e9799fa0479987199d8c104
erastoff <yuriy.erastov@yandex.ru>
2022-10-24 09:10:50 +0300

    settings.py updated

--- nursery_site/nursery_site/settings.py
+++ nursery_site/nursery_site/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 # Application definition
 
@@ -37,6 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    #DjDT
+    "debug_toolbar",
+
+    # my
+    "parents.apps.ParentsConfig",
+    "puppy.apps.PuppyConfig",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "nursery_site.urls"
