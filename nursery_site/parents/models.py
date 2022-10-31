424d3ef468582371bf43cf495466395728642f5e
erastoff <yuriy.erastov@yandex.ru>
2022-10-31 19:41:59 +0300

    models.py updated

--- nursery_site/parents/models.py
+++ nursery_site/parents/models.py
@@ -27,24 +27,28 @@ class ParentColor(models.Model):
 
 class Parent(models.Model):
     name = models.CharField(max_length=64)
-    breed = models.ForeignKey(ParentBreed, on_delete=models.PROTECT, related_name="parent")
-    gender = models.ForeignKey(ParentGender, on_delete=models.PROTECT, related_name="parent")
-    color = models.ForeignKey(ParentColor, on_delete=models.PROTECT, related_name="parent")
     birth = models.DateTimeField()
     height = models.FloatField()
     weight = models.FloatField()
     description = models.TextField(blank=True, null=False)
 
+    breed = models.ForeignKey(
+        ParentBreed, on_delete=models.PROTECT, related_name="parent", null=True
+    )
+    gender = models.ForeignKey(
+        ParentGender, on_delete=models.PROTECT, related_name="parent", null=True
+    )
+    color = models.ForeignKey(
+        ParentColor, on_delete=models.PROTECT, related_name="parent", null=True
+    )
+
     def __str__(self):
         return self.name
-
-    # kind = models.ForeignKey(AnimalKind, on_delete=models.PROTECT, related_name="animal")
-    # food = models.ManyToManyField("parents.AnimalFood", related_name="parents")
 
 
 class ParentKennel(models.Model):
     dog = models.OneToOneField(Parent, on_delete=models.CASCADE, related_name="kennel")
-    number = models.PositiveIntegerField(unique=True)
+    number = models.PositiveIntegerField(unique=True, null=True)
     description = models.TextField(blank=True, null=False)
 
     def __str__(self):
