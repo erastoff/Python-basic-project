37ed1cfef20887d0162091808f0c95e6e1118c66
erastoff <yuriy.erastov@yandex.ru>
2022-10-31 19:41:45 +0300

    admin.py updated

--- nursery_site/parents/admin.py
+++ nursery_site/parents/admin.py
@@ -7,16 +7,21 @@ from .models import Parent, ParentBreed, ParentGender, ParentColor, ParentKennel
 class UniversalAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description"
     list_display_links = "pk", "name"
+    ordering = ("pk",)
 
 
 @admin.register(Parent)
 class ParentAdmin(admin.ModelAdmin):
-    list_display = "pk", "name", "birth", "description"
+    list_display = "pk", "name", "breed", "gender", "birth"
     list_display_links = "pk", "name"
-    ordering = "pk",
+    ordering = ("pk",)
 
 
 @admin.register(ParentKennel)
 class ParentKennelAdmin(admin.ModelAdmin):
     list_display = "pk", "number", "description"
-    list_display_links = "pk", "number",
+    list_display_links = (
+        "pk",
+        "number",
+    )
+    ordering = ("pk",)
