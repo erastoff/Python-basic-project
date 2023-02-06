8f8e3688f96abde807c6bb800eec3d5341abc83b
erastoff <yuriy.erastov@yandex.ru>
2023-02-06 21:13:25 +0300

    wsgi.py changed

--- nursery_site/nursery_site/wsgi.py
+++ nursery_site/nursery_site/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, django
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nursery_site.settings")
+
+django.setup()
 
 application = get_wsgi_application()
