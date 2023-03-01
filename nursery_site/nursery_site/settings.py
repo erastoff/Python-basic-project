cc00000e19948207746a921963251588ea74f053
erastoff <yuriy.erastov@yandex.ru>
2023-03-01 08:41:13 +0300

    settings.py changed

--- nursery_site/nursery_site/settings.py
+++ nursery_site/nursery_site/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     # my
     "parents.apps.ParentsConfig",
     "puppy.apps.PuppyConfig",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH
+LOGIN_REDIRECT_URL = "/parents/"
+LOGIN_URL = "/users/login/"
+LOGOUT_REDIRECT_URL = "/parents/"
